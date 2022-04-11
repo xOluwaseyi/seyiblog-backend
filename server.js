ba59959a06c48691c54fc6fc1374d1deee1b2580
Fagbemi Oluwaseyi <seyifagbemi211@gmail.com>
2022-04-11 16:31:55 +0100

    backend update

--- server.js
+++ server.js
@@ -1,26 +1,10 @@
-// const jsonServer = require('json-server');
-// const server = jsonServer.create();
-// const router = jsonServer.router('./data/db.json');
-// const middlewares = jsonServer.defaults();
-// const port = process.env.PORT || 8080;
-
-// server.use(middlewares);
-// server.use(router);
-
-// server.listen(port);
-
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("./db.json");
-const middlewares = jsonServer.defaults({
-  static: "./build",
-});
-const PORT = process.env.PORT || 8000;
+const router = jsonServer.router("./data/db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080;
+
 server.use(middlewares);
-server.use(
-  jsonServer.rewriter({
-    "/blogs/*": "/$1",
-  })
-);
 server.use(router);
-server.listen(PORT);
+
+server.listen(port);
